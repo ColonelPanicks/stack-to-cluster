2e448da83a024993e019dc782c490e8a8e5e531d
stuts <stu.j.franks@gmail.com>
2021-03-24 16:41:06 +0000

    Automatically accept yum commands

--- run.sh
+++ run.sh
@@ -13,16 +13,16 @@ sudo mkdir -p $packagedir
 sudo chmod 777 $packagedir
 
 # Download user-suite packages
-sudo yum install https://repo.openflighthpc.org/pub/centos/7/openflighthpc-release-latest.noarch.rpm
-yumdownloader --destdir=$packagedir --resolve flight-user-suite
+sudo yum install -y https://repo.openflighthpc.org/pub/centos/7/openflighthpc-release-latest.noarch.rpm
+yumdownloader --destdir=$packagedir --resolve -y flight-user-suite
 
 # Install & configure this node
-sudo yum install $packagedir/*.rpm
+sudo yum install -y $packagedir/*.rpm
 /opt/flight/bin/flight config set cluster.name $clustername
 
 # Install & configure compute nodes
 for node in $nodes ; do 
-    ssh $node "sudo yum install $packagedir/*.rpm"
+    ssh $node "sudo yum install -y $packagedir/*.rpm"
     ssh $node "/opt/flight/bin/flight config set cluster.name $clustername"
 done
 
